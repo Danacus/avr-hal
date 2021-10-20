4777161c440154edbb49d2e052f0330a3fa0703c
Daan Vanoverloop <daan@vanoverloop.xyz>
2021-10-20 20:05:00 +0200

    Unfix

--- arduino-hal/src/lib.rs
+++ arduino-hal/src/lib.rs
@@ -202,19 +202,7 @@ macro_rules! pins {
     };
 }
 
-#[cfg(any(feature = "lora32u4ii"))]
-#[macro_export]
-macro_rules! default_serial {
-    ($p:expr, $pins:expr, $baud:expr) => {
-        $crate::Usart::new(
-            $p.USART0,
-            $pins.d0,
-            $pins.d1.into_output(),
-            $crate::hal::usart::BaudrateExt::into_baudrate($baud),
-        )
-    };
-}
-#[cfg(any(feature = "arduino-leonardo"))]
+#[cfg(any(feature = "arduino-leonardo", feature = "lora32u4ii"))]
 #[macro_export]
 macro_rules! default_serial {
     ($p:expr, $pins:expr, $baud:expr) => {
